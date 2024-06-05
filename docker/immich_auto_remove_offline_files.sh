1390c8e1f104cccf36a32855370bab88d0344f4e
Radoslav <cobinet59@gmail.com>
2024-06-05 15:28:26 +0000

    fixed entrypoint

--- docker/immich_auto_remove_offline_files.sh
+++ docker/immich_auto_remove_offline_files.sh
@@ -3,4 +3,4 @@
 args="--no_prompt --admin_apikey $API_KEY_ADMIN --user_apikey $API_KEY_USER --immichaddress $API_URL"
 
 BASEDIR=$(dirname "$0")
-echo $args | xargs python3 -u $BASEDIR/immich_auto_album.py
+echo $args | xargs python3 -u $BASEDIR/immich_auto_remove_offline_files.py
