eb87649e6a139872c313913efc8c6bf408910dda
Radoslav <cobinet59@gmail.com>
2024-07-24 18:48:40 +0000

    rewrote script for API > v106

--- immich_auto_remove_offline_files.py
+++ immich_auto_remove_offline_files.py
@@ -3,47 +3,33 @@
 import argparse
 import requests
 import logging, sys
+from datetime import datetime
+
+import requests
+from requests.adapters import HTTPAdapter
+from urllib3.util.retry import Retry
 from urllib.parse import urlparse
 
+class DateTimeFormatter(logging.Formatter):
+  def formatTime(self, record, datefmt=None):
+    dt = datetime.fromtimestamp(record.created)
+    if datefmt:
+      s = dt.strftime(datefmt)
+    else:
+      s = dt.isoformat(sep=' ', timespec='seconds')
+    return s
 
-class CustomLogger(logging.Logger):
-  def __init__(self, name, level=logging.NOTSET, stream=sys.stdout):
-    super().__init__(name, level)
+logging.basicConfig(
+  stream=sys.stdout, 
+  level=logging.INFO, 
+  format='%(asctime)s - %(levelname)s - %(message)s'
+)
 
-  def set_indent_level(self, indent_level):
-    for handler in self.handlers:
-      if isinstance(handler, IndentHandler):
-        handler.indent_level = indent_level
-      return self
+for handler in logging.getLogger().handlers:
+  handler.setFormatter(DateTimeFormatter(
+    fmt='%(asctime)s - %(levelname)s - %(message)s'
+  ))
 
-  def log_with_indent(self, level, msg, *args, **kwargs):
-    self.log(level, msg, *args, **kwargs)
-    return self
-
-class IndentHandler(logging.StreamHandler):
-  def __init__(self, *args, **kwargs):
-    super().__init__(*args, **kwargs)
-    self.indent_level = 0
-        
-  def set_indent_level(self, indent_level):
-    self.indent_level = indent_level
-
-  def emit(self, record):
-    record.msg = '   ' * self.indent_level + '└─ ' + record.msg
-    super().emit(record)
-
-def custom_logger():
-  log_format = '%(asctime)s - %(levelname)s - %(message)s'
-  
-  logger = CustomLogger(__name__)
-  
-  formatter = logging.Formatter(log_format, datefmt='%Y-%m-%d %H:%M:%S')
-  
-  handler = IndentHandler(sys.stdout)
-  handler.setFormatter(formatter)
-  logger.addHandler(handler)
-  
-  return logger
 
 def parse_arguments():
   parser = argparse.ArgumentParser(description='Fetch file report and delete orphaned media assets from Immich.')
@@ -52,17 +38,100 @@ def parse_arguments():
   parser.add_argument('--offline_threshold', help='If offline files exceed threshold, cleaner will not proceed with cleaning', nargs='?', default=100)
   return parser.parse_args()
 
+class Immich():
+  def __init__(self, url: str, key: str):
+    self.api_url = f'{urlparse(url).scheme}://{urlparse(url).netloc}/api'
+    self.headers = {
+      'x-api-key': key,
+      'Accept': 'application/json'
+    }
+    self.assets = list()
+    self.libraries = list()
+  
+  def fetchAssets(self, size: int = 1000, page: int = 1) -> list:
+    payload = {
+      'size' : size,
+      'page' : page
+    }
+    assets_total = list()
+    assets_received = size
+
+    logging.info(f'⬇️  Fetching assets: ')
+    logging.info(f'   Page size: {size}')
+
+    while assets_received == size:
+
+      session = requests.Session()
+      retry = Retry(connect=3, backoff_factor=0.5)
+      adapter = HTTPAdapter(max_retries=retry)
+      session.mount('http://', adapter)
+      session.mount('https://', adapter)
+
+      response = session.post(f"{self.api_url}/search/metadata", headers=self.headers, json=payload)
+
+      if not response.ok:
+        logging.error('   Error:', response.status_code, response.text)
+
+      assets_total = assets_total + response.json()['assets']['items']
+      payload["page"] += 1
+      assets_received = len(response.json()['assets']['items'])
+    
+    self.assets = assets_total
+    
+    logging.info(f'   Pages: {payload["page"]}')   
+    logging.info(f'   Assets: {len(self.assets)}')
+    
+    return self.assets
+
+  def fetchLibraries(self) -> list:
+    logging.info('⬇️  Fetching libraries: ')
+
+    session = requests.Session()
+    retry = Retry(connect=3, backoff_factor=0.5)
+    adapter = HTTPAdapter(max_retries=retry)
+    session.mount('http://', adapter)
+    session.mount('https://', adapter)
+
+    response = session.get(f"{self.api_url}/libraries", headers=self.headers)
+
+    if not response.ok:
+      logging.error('   Error:', response.status_code, response.text)
+
+    self.libraries = response.json()
+
+    for lib in self.libraries:
+      logging.info(f'     {lib["id"]} {lib["name"]}')
+    logging.info(f'   Libraries: {len(self.libraries)}')
+
+    return self.libraries
+
+  def removeOfflineFiles(self, library_id: str) -> None:
+    session = requests.Session()
+    retry = Retry(connect=3, backoff_factor=0.5)
+    adapter = HTTPAdapter(max_retries=retry)
+    session.mount('http://', adapter)
+    session.mount('https://', adapter)
+
+    response = session.post(f"{self.api_url}/libraries/{library_id}/removeOffline", headers=self.headers)
+
+    if response.ok:
+      logging.info("  🟢 Success!")
+    else:
+      logging.error(f"  🔴 Error! {response.status_code} {response.text}") 
+
 def main():
-
-  logger = custom_logger()
-
   args = parse_arguments()
+
+
 
   # Prompt for admin API key if not provided
   api_key = args.api_key if args.api_key else input('Enter the Immich API key: ')
 
   # Prompt for Immich API address if not provided
   api_url = args.api_url if args.api_url else input('Enter the full web address for Immich, including protocol and port: ')
+
+  # Set default offline_threshold of 50 files. If there are More than 50 files missing, the cleaner will skip the cleaning and issue a warning. This is in case the External Library goes offline and all assets get flagged.
+  offline_threshold = args.offline_threshold if args.offline_threshold else 50
 
   if not api_key:
     print("API key is required")
@@ -71,86 +140,33 @@ def main():
     print("API URL is required")
     return
 
-  immich_parsed_url = urlparse(api_url)
-  immich_base_url = f'{immich_parsed_url.scheme}://{immich_parsed_url.netloc}'
-  immich_api_url = f'{immich_base_url}/api'
-  immich_api_url_ass = f'{immich_api_url}/asset'
-  immich_api_url_libs = f'{immich_api_url}/library'
+  logging.info('============== INITIALIZING ==============')
   
-  headers = {
-    'x-api-key': api_key,
-    'Accept': 'application/json'
-  }
+  immich = Immich(api_url, api_key)
+  immich.fetchLibraries()
+  immich.fetchAssets()
 
-  logger.set_indent_level(0).log_with_indent(logging.INFO, f'Retrieving list of Immich libraries...')
-  
-  try:
-    response_libraries = requests.request("GET", immich_api_url_libs, headers=headers)
-    response_libraries.raise_for_status()
-
-    external_libraries = [x for x in response_libraries.json() if x["type"] == "EXTERNAL"]
-
-    logger.set_indent_level(0).log_with_indent(
-      logging.INFO, f'{len(external_libraries)} external libraries found.'
-    )
+  for library in immich.libraries:
     
-    for library in external_libraries:
-
-      logger.set_indent_level(1).log_with_indent(
-        logging.INFO, f'Searching for Offline files in {library["name"]} ({library["id"]})...'
-      )
-      
-      response_assets = requests.request("GET", immich_api_url_ass, headers=headers)
-      response_assets.raise_for_status()
-      
-      offline_files = [x for x in response_assets.json() if x["isOffline"] == True and x["libraryId"] == library["id"]]
-
-      logger.set_indent_level(2).log_with_indent(
-        logging.INFO, f'{len(offline_files)} offline files found.'
-      )
-      
-      for asset in offline_files:
-
-        logger.set_indent_level(3).log_with_indent(
-          logging.INFO, f'{asset["originalFileName"]} ({asset["id"]}) {asset["originalPath"]}'
-        )
-
-      if len(offline_files) == 0:
-        logger.set_indent_level(2).log_with_indent(
-          logging.INFO, f'➡️  Skipping cleaning beacuse there are no offline files.'
-        )
-  
-      elif len(offline_files) >= int(args.offline_threshold):
-        logger.set_indent_level(2).log_with_indent(
-          logging.WARNING, f'⚠️  There are {len(offline_files)} offline files which is more than the desired threshold of {args.offline_threshold}! Skipping cleaning! Check if the external libraries are available before manual cleaning!'
-        )
-
-      else:
-        logger.set_indent_level(2).log_with_indent(
-          logging.INFO, f'🗑️  Attempting to clean library...'
-        )
-
-        immich_api_url_offline = f'{immich_api_url}/library/{library["id"]}/removeOffline'
-
-        try:
-          cleaning_response = requests.request("POST", immich_api_url_offline, headers=headers)
-          cleaning_response.raise_for_status()
+    offline = 0
+    logging.info(f'Offline Assets in "{library["name"]}":')
+    
+    for asset in immich.assets:
+      if asset["libraryId"] == library["id"]:
+        if asset["isOffline"] == True:
           
-          logger.set_indent_level(3).log_with_indent(
-            logging.INFO, f'🟢 Cleaned library "{library["name"]}"'
-          )
-        
-        except requests.exceptions.RequestException as e:
-          logger.set_indent_level(3).log_with_indent(
-            logging.ERROR, f'🔴 Failed to clean library "{library["name"]}": \n{str(e)}'
-          )
-          return
-  
-  except requests.exceptions.RequestException as e:
-    logger.set_indent_level(1).log_with_indent(
-      logging.ERROR, f'Failed to fetch libraries: \n{str(e)}'
-    )
-    return
+          offline += 1
+          logging.info(f'     ⭕ {asset["id"]} {asset["originalPath"]}')
+
+    if offline >= int(offline_threshold):
+      logging.warning(f'⚠️  There are {offline} offline files which is more than the desired threshold of {offline_threshold}! Skipping cleaning! Check if the external libraries are available before manual cleaning!')
+    
+    elif offline > 0:
+      logging.info(f'  🚮 Removing {offline} files.')
+      immich.removeOfflineFiles(library["id"]);
+    
+    else:
+      logging.info(f'  ➡️  Skipping cleaning beacuse there are no offline files.')
 
 if __name__ == '__main__':
   main()
